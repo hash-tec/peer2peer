d35b62adcc435927e4ad8b3c7b6729e0d6ebbcdd
hashry <hashtech03@gmail.com>
2024-06-29 16:14:54 +0100

    Added a new field to bthe create model to accept the item image from the user👍

--- marketplace/models.py
+++ marketplace/models.py
@@ -8,6 +8,6 @@ class CreateListing(models.Model):
     description = models.TextField(blank=False)
     price = models.DecimalField( max_digits=10, decimal_places=2)
     date_created = models.DateField(auto_now_add=True)
-
+    image = models.FileField(upload_to="items_images", null=True)
     def __str__(self):
         return f"{self.item_name} "
