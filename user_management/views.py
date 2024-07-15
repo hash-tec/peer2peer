c9d72fa5115d42431a0975026996460c840ad166
hashry <hashtech03@gmail.com>
2024-07-15 23:27:59 +0100

    Added some css and some logic

--- user_management/views.py
+++ user_management/views.py
@@ -59,6 +59,7 @@ class ProfileView(TemplateView):
     template_name = "user_management/profile.html"
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        context["verify_user"] = kwargs["username"]
         context["username"] = self.request.user.username
         context["lastname"] = self.request.user.last_name
         context["email"] = self.request.user.email
