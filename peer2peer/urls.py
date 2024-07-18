17e1fa37d09e60c27da7d4ba3d6aa4addec146be
hashry <hashtech03@gmail.com>
2024-07-18 08:03:59 +0100

    Started cart application

--- peer2peer/urls.py
+++ peer2peer/urls.py
@@ -22,5 +22,6 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('user_management.urls')),
-    path('marketplace/', include('marketplace.urls'))
+    path('marketplace/', include('marketplace.urls')),
+    path('cart/', include('cart.urls') )
 ]+ static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
